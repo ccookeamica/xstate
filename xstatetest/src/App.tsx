0df4a8b266f90ba5132729769306a66f29047c56
ccookeamica <ccooke@amica.com>
2020-03-11 10:17:26 -0400

    Updates

--- xstatetest/src/App.tsx
+++ xstatetest/src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useMachine } from "@xstate/react";
+import { lightMachine } from "./TrafficMachine";
+import "./App.css";
 
 function App() {
+  const [state, send] = useMachine(lightMachine);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <output className="lightBox">
+          <div
+            className={
+              (state.context.light !== "green" && "off") + " light green"
+            }
+          ></div>
+          <div
+            className={
+              (state.context.light !== "yellow" && "off") + " light yellow"
+            }
+          ></div>
+          <div
+            className={
+              (state.context.light === "red"
+                ? "red"
+                : state.context.light === "redblink"
+                ? "redblink"
+                : "off") + " light"
+            }
+          ></div>
+        </output>
+        <div className="buttonBox">
+          <button
+            disabled={!!state.context.outage}
+            onClick={() => send("CHANGE")}
+          >
+            Change Light
+          </button>
+          <button disabled={state.context.light !== "red"}>Toggle Walk</button>
+          <button
+            onClick={() => send("OUTAGE", { value: !state.context.outage })}
+          >
+            Toggle Outage
+          </button>
+        </div>
       </header>
     </div>
   );
